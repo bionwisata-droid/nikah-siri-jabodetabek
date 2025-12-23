a393241eee434ebc50c1814675a0888c84c4e4e2
nikah-sirri <bionwisata@gmail.com>
2025-12-23 15:03:07 +0700

    Add files via upload

--- view.js
+++ view.js
@@ -1,38 +1,6 @@
 import * as __WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__ from "@wordpress/interactivity";
-/******/ var __webpack_modules__ = ({
-
-/***/ 438:
-/***/ ((module) => {
-
-module.exports = import("@wordpress/interactivity-router");;
-
-/***/ })
-
-/******/ });
-/************************************************************************/
-/******/ // The module cache
-/******/ var __webpack_module_cache__ = {};
-/******/ 
-/******/ // The require function
-/******/ function __webpack_require__(moduleId) {
-/******/ 	// Check if module is in cache
-/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 	if (cachedModule !== undefined) {
-/******/ 		return cachedModule.exports;
-/******/ 	}
-/******/ 	// Create a new module (and put it into the cache)
-/******/ 	var module = __webpack_module_cache__[moduleId] = {
-/******/ 		// no module.id needed
-/******/ 		// no module.loaded needed
-/******/ 		exports: {}
-/******/ 	};
-/******/ 
-/******/ 	// Execute the module function
-/******/ 	__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 
-/******/ 	// Return the exports of the module
-/******/ 	return module.exports;
-/******/ }
+/******/ // The require scope
+/******/ var __webpack_require__ = {};
 /******/ 
 /************************************************************************/
 /******/ /* webpack/runtime/define property getters */
@@ -61,48 +29,153 @@ var x = (y) => {
 } 
 var y = (x) => (() => (x))
 const interactivity_namespaceObject = x({ ["getContext"]: () => (__WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__.getContext), ["getElement"]: () => (__WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__.getElement), ["store"]: () => (__WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__.store), ["withSyncEvent"]: () => (__WEBPACK_EXTERNAL_MODULE__wordpress_interactivity_8e89b257__.withSyncEvent) });
-;// ./node_modules/@wordpress/block-library/build-module/query/view.js
+;// ./node_modules/@wordpress/block-library/build-module/navigation/view.js
 
-const isValidLink = (ref) => ref && ref instanceof window.HTMLAnchorElement && ref.href && (!ref.target || ref.target === "_self") && ref.origin === window.location.origin;
-const isValidEvent = (event) => event.button === 0 && // Left clicks only.
-!event.metaKey && // Open in new tab (Mac).
-!event.ctrlKey && // Open in new tab (Windows).
-!event.altKey && // Download.
-!event.shiftKey && !event.defaultPrevented;
-(0,interactivity_namespaceObject.store)(
-  "core/query",
+const focusableSelectors = [
+  "a[href]",
+  'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+  "select:not([disabled]):not([aria-hidden])",
+  "textarea:not([disabled]):not([aria-hidden])",
+  "button:not([disabled]):not([aria-hidden])",
+  "[contenteditable]",
+  '[tabindex]:not([tabindex^="-"])'
+];
+document.addEventListener("click", () => {
+});
+const { state, actions } = (0,interactivity_namespaceObject.store)(
+  "core/navigation",
   {
+    state: {
+      get roleAttribute() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        return ctx.type === "overlay" && state.isMenuOpen ? "dialog" : null;
+      },
+      get ariaModal() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        return ctx.type === "overlay" && state.isMenuOpen ? "true" : null;
+      },
+      get ariaLabel() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        return ctx.type === "overlay" && state.isMenuOpen ? ctx.ariaLabel : null;
+      },
+      get isMenuOpen() {
+        return Object.values(state.menuOpenedBy).filter(Boolean).length > 0;
+      },
+      get menuOpenedBy() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        return ctx.type === "overlay" ? ctx.overlayOpenedBy : ctx.submenuOpenedBy;
+      }
+    },
     actions: {
-      navigate: (0,interactivity_namespaceObject.withSyncEvent)(function* (event) {
+      openMenuOnHover() {
+        const { type, overlayOpenedBy } = (0,interactivity_namespaceObject.getContext)();
+        if (type === "submenu" && // Only open on hover if the overlay is closed.
+        Object.values(overlayOpenedBy || {}).filter(Boolean).length === 0) {
+          actions.openMenu("hover");
+        }
+      },
+      closeMenuOnHover() {
+        const { type, overlayOpenedBy } = (0,interactivity_namespaceObject.getContext)();
+        if (type === "submenu" && // Only close on hover if the overlay is closed.
+        Object.values(overlayOpenedBy || {}).filter(Boolean).length === 0) {
+          actions.closeMenu("hover");
+        }
+      },
+      openMenuOnClick() {
         const ctx = (0,interactivity_namespaceObject.getContext)();
         const { ref } = (0,interactivity_namespaceObject.getElement)();
-        const queryRef = ref.closest(
-          ".wp-block-query[data-wp-router-region]"
-        );
-        if (isValidLink(ref) && isValidEvent(event)) {
-          event.preventDefault();
-          const { actions } = yield Promise.resolve(/* import() */).then(__webpack_require__.bind(__webpack_require__, 438));
-          yield actions.navigate(ref.href);
-          ctx.url = ref.href;
-          const firstAnchor = `.wp-block-post-template a[href]`;
-          queryRef.querySelector(firstAnchor)?.focus();
+        ctx.previousFocus = ref;
+        actions.openMenu("click");
+      },
+      closeMenuOnClick() {
+        actions.closeMenu("click");
+        actions.closeMenu("focus");
+      },
+      openMenuOnFocus() {
+        actions.openMenu("focus");
+      },
+      toggleMenuOnClick() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        const { ref } = (0,interactivity_namespaceObject.getElement)();
+        if (window.document.activeElement !== ref) {
+          ref.focus();
+        }
+        const { menuOpenedBy } = state;
+        if (menuOpenedBy.click || menuOpenedBy.focus) {
+          actions.closeMenu("click");
+          actions.closeMenu("focus");
+        } else {
+          ctx.previousFocus = ref;
+          actions.openMenu("click");
+        }
+      },
+      handleMenuKeydown: (0,interactivity_namespaceObject.withSyncEvent)((event) => {
+        const { type, firstFocusableElement, lastFocusableElement } = (0,interactivity_namespaceObject.getContext)();
+        if (state.menuOpenedBy.click) {
+          if (event.key === "Escape") {
+            event.stopPropagation();
+            actions.closeMenu("click");
+            actions.closeMenu("focus");
+            return;
+          }
+          if (type === "overlay" && event.key === "Tab") {
+            if (event.shiftKey && window.document.activeElement === firstFocusableElement) {
+              event.preventDefault();
+              lastFocusableElement.focus();
+            } else if (!event.shiftKey && window.document.activeElement === lastFocusableElement) {
+              event.preventDefault();
+              firstFocusableElement.focus();
+            }
+          }
         }
       }),
-      *prefetch() {
-        const { ref } = (0,interactivity_namespaceObject.getElement)();
-        if (isValidLink(ref)) {
-          const { actions } = yield Promise.resolve(/* import() */).then(__webpack_require__.bind(__webpack_require__, 438));
-          yield actions.prefetch(ref.href);
+      handleMenuFocusout: (0,interactivity_namespaceObject.withSyncEvent)((event) => {
+        const { modal, type } = (0,interactivity_namespaceObject.getContext)();
+        if (event.relatedTarget === null || !modal?.contains(event.relatedTarget) && event.target !== window.document.activeElement && type === "submenu") {
+          actions.closeMenu("click");
+          actions.closeMenu("focus");
+        }
+      }),
+      openMenu(menuOpenedOn = "click") {
+        const { type } = (0,interactivity_namespaceObject.getContext)();
+        state.menuOpenedBy[menuOpenedOn] = true;
+        if (type === "overlay") {
+          document.documentElement.classList.add("has-modal-open");
+        }
+      },
+      closeMenu(menuClosedOn = "click") {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
+        state.menuOpenedBy[menuClosedOn] = false;
+        if (!state.isMenuOpen) {
+          if (ctx.modal?.contains(window.document.activeElement)) {
+            ctx.previousFocus?.focus();
+          }
+          ctx.modal = null;
+          ctx.previousFocus = null;
+          if (ctx.type === "overlay") {
+            document.documentElement.classList.remove(
+              "has-modal-open"
+            );
+          }
         }
       }
     },
     callbacks: {
-      *prefetch() {
-        const { url } = (0,interactivity_namespaceObject.getContext)();
+      initMenu() {
+        const ctx = (0,interactivity_namespaceObject.getContext)();
         const { ref } = (0,interactivity_namespaceObject.getElement)();
-        if (url && isValidLink(ref)) {
-          const { actions } = yield Promise.resolve(/* import() */).then(__webpack_require__.bind(__webpack_require__, 438));
-          yield actions.prefetch(ref.href);
+        if (state.isMenuOpen) {
+          const focusableElements = ref.querySelectorAll(focusableSelectors);
+          ctx.modal = ref;
+          ctx.firstFocusableElement = focusableElements[0];
+          ctx.lastFocusableElement = focusableElements[focusableElements.length - 1];
+        }
+      },
+      focusFirstElement() {
+        const { ref } = (0,interactivity_namespaceObject.getElement)();
+        if (state.isMenuOpen) {
+          const focusableElements = ref.querySelectorAll(focusableSelectors);
+          focusableElements?.[0]?.focus();
         }
       }
     }
